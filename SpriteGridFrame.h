e2e82a09497d12ced38849ad899143ef4cc9032b
Lucas Pottier <78223918+LuKP17@users.noreply.github.com>
2024-03-24 11:22:25 +0100

    uiPaintBox test result

--- SpriteGridFrame.h
+++ SpriteGridFrame.h
@@ -19,27 +19,31 @@ struct SpriteGridFrame : public uiFrame
 		windowStyle().borderSize	 = 0;
 		frameProps().resizeable		 = false;
 
+		/* WORKS, THE PAINTBOX IS DRAWN AT THE RIGHT POS/SIZE */
 		paintBox = new uiPaintBox(this, clientPos(), clientSize());
 		paintBox->anchors().right = true;
 		paintBox->anchors().bottom = true;
-		paintBox->onPaint = [&](Rect const & r) { onPaintPaintBox(r); };
 
-		paintBox->repaint(); // TEST I guess this calls onPaintPaintBox()
+		// called when making another frame active, then also when making the grid frame active, and when clicking on Clear button
+		// IT'S FINE, BUT I WANT TO CALL IT WHEN THE USER CLICKS IN THE FRAME
+		paintBox->onPaint = [&](Rect const & r) { onPaintPaintBox(r); };
 
 		this->appData = appData;
 	}
 
 	void onPaintPaintBox(Rect const & r)
 	{
+		Serial.printf("Entered custom paint function\n");
 		/* Perform custom drawings */
+		// DOESN'T DRAW SHIT
 
 		int w = r.width(), h = r.height();
 
 		auto cv = canvas();
 
 		// TEST I hope the sprite grid will be red
-		cv->setPenColor(Color::BrightRed);
-		cv->fillRectangle(r)
+		cv->setPenColor(Color::BrightGreen);
+		cv->fillRectangle(r);
 	}
 
 	void clear()
